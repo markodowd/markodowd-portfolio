2a492b9c8edbc4be3570c6bfabcbee78c5857d3d
Mark O'Dowd <contact@markodowd.dev>
2026-01-04 19:52:41 +0000

    feat: skills data

--- components/skills/skills-data.ts
+++ components/skills/skills-data.ts
@@ -16,6 +16,8 @@ export const skills: Skill[] = [
   { name: "Express", icon: "siExpress", category: "Backend" },
   { name: "Django", icon: "siDjango", category: "Backend" },
   { name: "Python", icon: "siPython", category: "Backend" },
+  { name: "PostgreSQL", icon: "siPostgresql", category: "Backend" },
+  { name: "SQL", icon: "siSql", category: "Backend" },
   
   // DevOps
   { name: "AWS", icon: "siAmazonaws", category: "DevOps" },
